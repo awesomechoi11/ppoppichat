c4ecd84c08295fb84e9f1e61b4f35831ab6bed1d
brandon <brandonchoi2016@gmail.com>
2021-01-27 11:54:15 -0800

    state based login display

--- ppoppichat/src/home/login.js
+++ ppoppichat/src/home/login.js
@@ -212,89 +212,162 @@ export class Login extends React.Component {
 
     render() {
 
+        var loginPlatformLeft;
+
+        if (this.state.mode === 'signin') {
+            loginPlatformLeft = (
+                <motion.div
+                    key={'loggedOut'}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    className='signed-in'
+                >
+                    <div className='login-form-title dutch-white'>
+                        sign in
+                </div>
+                    <Formik
+                        initialValues={{
+                            email: '',
+                            password: '',
+                        }}
+                        onSubmit={(values) => {
+                            //need to implement email login
+                            console.log(values)
+
+                        }}
+                    >
+                        <Form id='login-form' >
+                            <div className="login-form-wrapper">
+                                <label className="login-form-label" htmlFor="email">EMAIL</label>
+                                <Field id="email" name="email" type="email" className="login-form" />
+                            </div>
+                            <div className="login-form-wrapper">
+                                <label className="login-form-label" htmlFor="password">PASSWORD</label>
+                                <Field id="password" name="password" type="password" className="login-form" />
+                            </div>
+                            {/* <button id='loginbutton' type="submit">Submit</button> */}
+                        </Form>
+                    </Formik>
+                    <div className='alternate-login'>
+                        <div className='alternate-login-label'>
+                            <span className='dutch-white'>OR</span> SIGN IN WITH:
+                    </div>
+                        <img className='alternate-login-png' src={gpng}
+                            alt='google login logo'
+                            onClick={this.googlesignin}
+                        />
+                        <img className='alternate-login-png' src={gitpng}
+                            alt='github logo'
+                            onClick={this.githubsignin}
+                        />
+                        <img className='alternate-login-png' src={anonpng} alt='anon mask' />
+                    </div>
+                    <div className='here-button'
+                        onClick={e => {
+                            this.setState({ mode: 'signup' })
+                        }}
+                    >
+                        <span>DONT</span> HAVE AN ACCOUNT? SIGN UP <span className='dutch-white'>HERE</span>
+                    </div>
+
+                </motion.div>
+            )
+        } else if (this.state.mode === 'signup') {
+            loginPlatformLeft = (
+                <motion.div
+                    key={'loggedOut'}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    className='signed-in'
+                >
+                    <div className='login-form-title dutch-white'>
+                        sign up
+                </div>
+                    <Formik
+                        initialValues={{
+                            email: '',
+                            password: '',
+                        }}
+                        onSubmit={(values) => {
+                            //need to implement email login
+                            console.log(values)
+
+                        }}
+                    >
+                        <Form id='login-form' >
+                            <div className="login-form-wrapper">
+                                <label className="login-form-label" htmlFor="email">EMAIL</label>
+                                <Field id="email" name="email" type="email" className="login-form" />
+                            </div>
+                            <div className="login-form-wrapper">
+                                <label className="login-form-label" htmlFor="password">PASSWORD</label>
+                                <Field id="password" name="password" type="password" className="login-form" />
+                            </div>
+                            {/* <button id='loginbutton' type="submit">Submit</button> */}
+                        </Form>
+                    </Formik>
+                    <div className='alternate-login'>
+                        <div className='alternate-login-label'>
+                            <span className='dutch-white'>OR</span> SIGN IN WITH:
+                    </div>
+                        <img className='alternate-login-png' src={gpng}
+                            alt='google login logo'
+                            onClick={this.googlesignin}
+                        />
+                        <img className='alternate-login-png' src={gitpng}
+                            alt='github logo'
+                            onClick={this.githubsignin}
+                        />
+                        <img className='alternate-login-png' src={anonpng} alt='anon mask' />
+                    </div>
+                    <div className='here-button'
+                        onClick={e => {
+                            this.setState({ mode: 'signin' })
+                        }}
+                    >
+                        <span>ALREADY</span> HAVE AN ACCOUNT? SIGN IN <span className='dutch-white'>HERE</span>
+                    </div>
+
+                </motion.div>
+            )
+        } else if (this.state.mode === 'loggedin') {
+            loginPlatformLeft = (
+                <motion.div
+                    key={'loggedIn'}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    className='signed-out'
+                >
+                    <div className='login-form-title dutch-white'>
+                        welcome!
+                </div>
+                    <div className='login-username-title'>
+                        USERNAME
+                </div>
+                    <div className='login-username'>
+                        {this.state.username}
+                    </div>
+                    <div className="here-button login-signout" onClick={this.signout}>
+                        <span>NOT</span> YOUR ACCOUNT? SIGN <span className='dutch-white'>OUT</span>
+                    </div>
+                </motion.div>
+            )
+        }
+
+
+
         return (
             <div
                 className="login app-page" >
                 <ToastContainer />
                 <div id='login-platform'>
                     <AnimatePresence>
-                        {!this.state.loggedIn ? (
-                            <div className='login-platform-left'>
-                                <motion.div
-                                    key={'loggedOut'}
-                                    initial={{ opacity: 0 }}
-                                    animate={{ opacity: 1 }}
-                                    exit={{ opacity: 0 }}
-                                    className='signed-in'
-                                >
-                                    <div className='login-form-title dutch-white'>
-                                        sign up
-                                    </div>
-                                    <Formik
-                                        initialValues={{
-                                            email: '',
-                                            password: '',
-                                        }}
-                                        onSubmit={(values) => {
-                                            //need to implement email login
-                                            console.log(values)
-
-                                        }}
-                                    >
-                                        <Form id='login-form' >
-                                            <div className="login-form-wrapper">
-                                                <label className="login-form-label" htmlFor="email">EMAIL</label>
-                                                <Field id="email" name="email" type="email" className="login-form" />
-                                            </div>
-                                            <div className="login-form-wrapper">
-                                                <label className="login-form-label" htmlFor="password">PASSWORD</label>
-                                                <Field id="password" name="password" type="password" className="login-form" />
-                                            </div>
-                                            {/* <button id='loginbutton' type="submit">Submit</button> */}
-                                        </Form>
-                                    </Formik>
-                                    <div className='alternate-login'>
-                                        <div className='alternate-login-label'>
-                                            <span className='dutch-white'>OR</span> SIGN IN WITH:
-                                        </div>
-                                        <img className='alternate-login-png' src={gpng}
-                                            onClick={this.googlesignin}
-                                        />
-                                        <img className='alternate-login-png' src={gitpng}
-                                            onClick={this.githubsignin}
-                                        />
-                                        <img className='alternate-login-png' src={anonpng} />
-                                    </div>
-                                    <div>
-                                        <span>DONT</span> HAVE AN ACCOUNT? SIGN UP <span className='dutch-white'>HERE</span>
-                                    </div>
-
-                                </motion.div>
-                            </div>
-                        ) : (
-                                <div className='login-platform-left'>
-                                    <motion.div
-                                        key={'loggedIn'}
-                                        initial={{ opacity: 0 }}
-                                        animate={{ opacity: 1 }}
-                                        exit={{ opacity: 0 }}
-                                        className='signed-out'
-                                    >
-                                        <div className='login-form-title dutch-white'>
-                                            welcome!
-                                        </div>
-                                        <div className='login-username-title'>
-                                            USERNAME
-                                        </div>
-                                        <div className='login-username'>
-                                            {this.state.username}
-                                        </div>
-                                        <div className="login-signout" onClick={this.signout}>
-                                            <span>NOT</span> YOUR ACCOUNT? SIGN <span className='dutch-white'>OUT</span>
-                                        </div>
-                                    </motion.div>
-                                </div>
-                            )}
+                        <div className='login-platform-left'>
+                            {loginPlatformLeft}
+                        </div>
                     </AnimatePresence>
                     <div className='login-platform-right'>
                         <span >
